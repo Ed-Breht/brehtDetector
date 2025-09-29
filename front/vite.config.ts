3daffafe62c4db88cc561f32ece26602eaac122b
eduardbreht <ebrekht@phoenixit.ru>
2025-09-29 20:57:02 +0300

    nginx4

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,36 +1,39 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
+import {VitePWA} from 'vite-plugin-pwa';
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
-
-    pwaAssets: {
-      disabled: false,
-      config: true,
+    preview: {
+        allowedHosts: true
     },
+    plugins: [react(), VitePWA({
+        registerType: 'prompt',
+        injectRegister: false,
 
-    manifest: {
-      name: 'brehtDetector',
-      short_name: 'brehtDetector',
-      description: 'App for YOLO recoginze',
-      theme_color: '#ffffff',
-    },
+        pwaAssets: {
+            disabled: false,
+            config: true,
+        },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+        manifest: {
+            name: 'brehtDetector',
+            short_name: 'brehtDetector',
+            description: 'App for YOLO recoginze',
+            theme_color: '#ffffff',
+        },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+        workbox: {
+            globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+            cleanupOutdatedCaches: true,
+            clientsClaim: true,
+        },
+
+        devOptions: {
+            enabled: false,
+            navigateFallback: 'index.html',
+            suppressWarnings: true,
+            type: 'module',
+        },
+    })],
 })
