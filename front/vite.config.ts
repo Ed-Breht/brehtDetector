83d9eff15ceb29291444d8785e7d0f75b746d8d0
eduardbreht <ebrekht@phoenixit.ru>
2025-09-29 21:09:54 +0300

    nginx4

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,11 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     preview: {
-        allowedHosts: ['brehtdetector.ru', 'localhost', '0.0.0.0']
-    },
-    server: {
-        host: '0.0.0.0',
-        port: 4173
+        port: 4173,
+        allowedHosts: true,
+        strictPort: true,
+        host: true,
     },
     plugins: [react(), VitePWA({
         registerType: 'prompt',
